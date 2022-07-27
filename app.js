136b22b84a4973c63aff9451c3964a9475136ff0
damirdine <damirdine@outlook.fr>
2022-07-27 18:56:49 +0200

    display city by country search

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 fetch('cities.json')
 .then(res => res.json())
-.then(json => app(json))
+.then(json => data = app(json))
 
+let selectCountry = document.querySelector('#country')
+let tableContent = document.querySelector('#cityData')
 
 function app(data){
-    let listCountry = getPays(data)
-    let selectCountry = document.querySelector('#country')
+    let listCountry = getCountry(data)
     displayList(listCountry,selectCountry)
+    selectCountry.addEventListener("change", ()=>displayCity(searchByCountry(this.event.target.value,data)))
 }
-
-function getPays(data){
+function getCountry(data){
     let listCountry = []
     data.forEach(ville => {
-        let pay = ville.countrycode.name
-        if(!listCountry.includes(pay)){
-            listCountry.push(pay)
+        let country = ville.countrycode.name
+        if(!listCountry.includes(country)){
+            listCountry.push(country)
         }
-    });
+    })
+    listCountry.sort()
     return listCountry
 }
+function searchByCountry(country,data){
+    let listCity = []
+    data.forEach(city => {
+        let cityCountry = city.countrycode.name
+        if(cityCountry==country){
+            listCity.push(city)
+        }
+    })
+    return listCity
+}
 
-function displayList(list,select){
+function displayList(list){
     list.forEach(country => {
-        select.innerHTML += `<option value="${country}">${country}</option>`
-    });
+        selectCountry.innerHTML += `<option value="${country}">${country}</option>`
+    })
+}
+function displayCity(list){
+    list.forEach(city => {
+        tableContent.innerHTML += `<tr>
+        <td>${city.name}</td>
+        <td>${city.population}</td>
+    </tr>`
+    })
 }
